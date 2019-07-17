11689d7bb3d5a501282491c9da18a9b3b7aae3f9
thetianshuhuang <thetianshuhuang@gmail.com>
2019-07-16 22:29:44 -0500

    Added weighting

--- gs_gpu/idw.py
+++ gs_gpu/idw.py
@@ -4,11 +4,20 @@ from .interpolate import BaseInterpolation
 
 
 _MOD_IDW_WEIGHT = """
-__device__ {f} weight_function({f} x1, {f} y1, {f} x2, {f} y2, {f} idp) {{
+/**
+ * Weight function kernel
+ * x1 : first point lat
+ * y1 : first point long
+ * x2 : second point lat
+ * y2 : second point long
+ * idp : IDW power parameter
+ */
+__device__ {f} weight_function({f} x1, {f} y1, {f} x2, {f} y2, {f} idp)
+{{
 
     {f} sum = pow(x1 - x2, 2) + pow(y1 - y2, 2);
 
-    if(sum < 0.000000001) {{ return pow(1000000000, idp); }}
+    if(sum < 0.000000001) {{ return pow(1000000000.0, idp); }}
     else {{ return pow(rsqrt(sum), idp); }}
 }}
 """
@@ -16,3 +25,9 @@ __device__ {f} weight_function({f} x1, {f} y1, {f} x2, {f} y2, {f} idp) {{
 
 class IDWInterpolation(BaseInterpolation):
     WEIGHT_FUNCTION = _MOD_IDW_WEIGHT
+
+    OTHER_ARGS = "{f} idp"
+    OTHER_WF_ARGS = "idp"
+
+    def get_other_args(self, idp=2):
+        return [self.FLOAT_TYPE(idp)]
--- gs_gpu/interpolate.py
+++ gs_gpu/interpolate.py
@@ -24,11 +24,27 @@ import numpy as np
 #
 
 _MOD_INTERP = """
+/**
+ * Interpolation kernel
+ * lat_arr : latitude array
+ * long_arr : longitude array
+ * val : value array
+ * weights : weights array; set to all ones if no weighting desired
+ * size : number of points
+ * res : result array
+ * width_px : result raster image width
+ * height_px : result raster image height
+ * lat_min : latitude value corresponding to image bottom
+ * long_min : longitude value corresponding to image left
+ * lat_max : latitude value corresponding to image top
+ * long_max : longitude value corresponding to image right
+ * {other_args} : other parameters
+ */
 __global__ void interpolate(
-        {f} *lat_arr, {f} *long_arr, {f} *val, int size,
+        {f} *lat_arr, {f} *long_arr, {f} *val, {f} *weights, int size,
         {f} *res, int width_px, int height_px,
         {f} lat_min, {f} long_min, {f} lat_max, {f} long_max,
-        {f} idp)
+        {other_args})
 {{
     int px_x = threadIdx.x + blockIdx.x * blockDim.x;
     int px_y = threadIdx.y + blockIdx.y * blockDim.y;
@@ -42,11 +58,11 @@ __global__ void interpolate(
         {f} total_weight;
         {f} weight;
 
-        for(int i = 0; i < size; i++) {{
-            weight = weight_function(
-                lat_arr[i], long_arr[i], lat_pt, long_pt, idp);
+        for(int idx = 0; idx < size; idx++) {{
+            weight = weights[idx] * weight_function(
+                lat_arr[idx], long_arr[idx], lat_pt, long_pt, idp);
             total_weight += weight;
-            acc += weight * val[i];
+            acc += weight * val[idx];
         }}
 
         res[px_x * width_px + px_y] = acc / total_weight;
@@ -119,9 +135,18 @@ class BaseInterpolation:
                 "function. If you are writing your own weight_function, make "
                 "sure to set the WEIGHT_FUNCTION attribute.")
 
-        self.__kernel = SourceModule(
-            (self.WEIGHT_FUNCTION + _MOD_INTERP).format(f=self.FLOAT_CTYPE)
-        ).get_function("interpolate")
+        # other_args : {type} param, {type} param, ...
+        # other_args_as_param : param, param, ...
+        kernel_src = (
+            self.WEIGHT_FUNCTION.format(
+                f=self.FLOAT_CTYPE) +
+            _MOD_INTERP.format(
+                other_args=self.OTHER_ARGS.format(f=self.FLOAT_CTYPE),
+                other_args_as_param=self.OTHER_WF_ARGS,
+                f=self.FLOAT_CTYPE)
+        )
+
+        self.__kernel = SourceModule(kernel_src).get_function("interpolate")
 
     def __get_grid(self, size):
         """Get grid dimensions for a GPU operation.
@@ -142,8 +167,8 @@ class BaseInterpolation:
             1)
 
     def interpolate(
-            self, latitudes, longitudes, values,
-            size=(1024, 1024), lat_range=(0, 1), long_range=(0, 1), idp=2):
+            self, latitudes, longitudes, values, weights=None,
+            size=(1024, 1024), lat_range=(0, 1), long_range=(0, 1), **kwargs):
         """Run interpolation
 
         Parameters
@@ -154,6 +179,8 @@ class BaseInterpolation:
             Longitude vector
         values : np.array
             Point values
+        weights : np.array or None
+            Point weights; if None, uses uniform weighting
         size : int[2]
             Output image size
         lat_range : float[2]
@@ -175,17 +202,23 @@ class BaseInterpolation:
             Generated rasterized interpolation image.
         """
 
+        # Weights should default to ones
+        if weights is None:
+            weights = np.ones(values.shape, dtype=self.FLOAT_TYPE)
+
         # Check shapes
         if(
                 (latitudes.shape[0] != longitudes.shape[0]) or
-                (values.shape[0] != latitudes.shape[0])):
+                (values.shape[0] != latitudes.shape[0]) or
+                (weights.shape[0] != values.shape[0])):
             raise TypeError("Coordinate dimension mismatch")
 
         # Check types
         if(
                 (latitudes.dtype != self.FLOAT_TYPE) or
                 (longitudes.dtype != self.FLOAT_TYPE) or
-                (values.dtype != self.FLOAT_TYPE)):
+                (values.dtype != self.FLOAT_TYPE) or
+                (weights.dtype != self.FLOAT_TYPE)):
             raise TypeError(
                 "Input data must be of type {}. To change the input type, set "
                 "the 'fsize' argument on initialization.".format(
@@ -195,22 +228,33 @@ class BaseInterpolation:
         lat_gpu = cuda.mem_alloc(latitudes.nbytes)
         long_gpu = cuda.mem_alloc(longitudes.nbytes)
         val_gpu = cuda.mem_alloc(values.nbytes)
+        weights_gpu = cuda.mem_alloc(weights.nbytes)
         cuda.memcpy_htod(lat_gpu, latitudes)
         cuda.memcpy_htod(long_gpu, longitudes)
         cuda.memcpy_htod(val_gpu, values)
+        cuda.memcpy_htod(weights_gpu, weights)
 
         # Set up result
         res_cpu = np.zeros(size, dtype=self.FLOAT_TYPE)
         res_gpu = cuda.mem_alloc(res_cpu.nbytes)
 
+        args = [
+            lat_gpu,
+            long_gpu,
+            val_gpu,
+            weights_gpu,
+            np.int32(latitudes.shape[0]),
+            res_gpu,
+            np.int32(res_cpu.shape[1]),
+            np.int32(res_cpu.shape[0]),
+            self.FLOAT_TYPE(lat_range[0]),
+            self.FLOAT_TYPE(long_range[0]),
+            self.FLOAT_TYPE(lat_range[1]),
+            self.FLOAT_TYPE(long_range[1]),
+        ] + self.get_other_args(**kwargs)
+
         # Run kernel
-        self.__kernel(
-            lat_gpu, long_gpu, val_gpu, np.int32(latitudes.shape[0]),
-            res_gpu, np.int32(res_cpu.shape[1]), np.int32(res_cpu.shape[0]),
-            self.FLOAT_TYPE(lat_range[0]), self.FLOAT_TYPE(long_range[0]),
-            self.FLOAT_TYPE(lat_range[1]), self.FLOAT_TYPE(long_range[1]),
-            self.FLOAT_TYPE(idp),
-            block=self.BLOCK_SIZE, grid=self.__get_grid(size))
+        self.__kernel(*args, block=self.BLOCK_SIZE, grid=self.__get_grid(size))
 
         # Fetch result
         cuda.memcpy_dtoh(res_cpu, res_gpu)
